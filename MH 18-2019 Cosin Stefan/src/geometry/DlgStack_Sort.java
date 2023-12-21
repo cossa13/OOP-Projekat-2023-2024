e3a27f67148ae4299c885186c9c59a4dde0a6582
stefancosin <stefancosin2000@gmail.com>
2023-12-21 20:51:58 +0100

    Stack and Sort

---
+++ MH 18-2019 Cosin Stefan/src/geometry/DlgStack_Sort.java
@@ -0,0 +1,245 @@
+package geometry;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class DlgStack_Sort extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textFieldRadius;
+	private JTextField textFieldX;
+	private JTextField textFieldY;
+	private int check_one, check_two, check_three;
+	private boolean flag_ok = false;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DlgStack_Sort dialog = new DlgStack_Sort();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public DlgStack_Sort() {
+		setModal(true);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{120, 0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{63, 0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblXcoordinate = new JLabel("X coordinate:");
+			lblXcoordinate.setFont(new Font("Tahoma", Font.BOLD, 12));
+			GridBagConstraints gbc_lblXcoordinate = new GridBagConstraints();
+			gbc_lblXcoordinate.anchor = GridBagConstraints.EAST;
+			gbc_lblXcoordinate.insets = new Insets(0, 0, 5, 5);
+			gbc_lblXcoordinate.gridx = 1;
+			gbc_lblXcoordinate.gridy = 1;
+			contentPanel.add(lblXcoordinate, gbc_lblXcoordinate);
+		}
+		{
+			textFieldX = new JTextField();
+			textFieldX.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+					char c = e.getKeyChar();
+					if(c < '0' || c > '9' || c == KeyEvent.VK_SPACE)
+					{
+						if(c != KeyEvent.VK_MINUS)
+						{
+							e.consume();
+						}
+					}
+				}
+			});
+			GridBagConstraints gbc_textFieldX = new GridBagConstraints();
+			gbc_textFieldX.insets = new Insets(0, 0, 5, 0);
+			gbc_textFieldX.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textFieldX.gridx = 2;
+			gbc_textFieldX.gridy = 1;
+			contentPanel.add(textFieldX, gbc_textFieldX);
+			textFieldX.setColumns(10);
+		}
+		{
+			JLabel lblYcoordinate = new JLabel("Y coordinate:");
+			lblYcoordinate.setFont(new Font("Tahoma", Font.BOLD, 12));
+			GridBagConstraints gbc_lblYcoordinate = new GridBagConstraints();
+			gbc_lblYcoordinate.anchor = GridBagConstraints.EAST;
+			gbc_lblYcoordinate.insets = new Insets(0, 0, 5, 5);
+			gbc_lblYcoordinate.gridx = 1;
+			gbc_lblYcoordinate.gridy = 2;
+			contentPanel.add(lblYcoordinate, gbc_lblYcoordinate);
+		}
+		{
+			textFieldY = new JTextField();
+			textFieldY.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+					char c = e.getKeyChar();
+					if(c < '0' || c > '9' || c == KeyEvent.VK_SPACE)
+					{
+						if(c != KeyEvent.VK_MINUS)
+						{
+							e.consume();
+						}
+					}
+				}
+			});
+			GridBagConstraints gbc_textFieldY = new GridBagConstraints();
+			gbc_textFieldY.insets = new Insets(0, 0, 5, 0);
+			gbc_textFieldY.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textFieldY.gridx = 2;
+			gbc_textFieldY.gridy = 2;
+			contentPanel.add(textFieldY, gbc_textFieldY);
+			textFieldY.setColumns(10);
+		}
+		{
+			JLabel lblRadius = new JLabel("Radius:");
+			lblRadius.setFont(new Font("Tahoma", Font.BOLD, 12));
+			GridBagConstraints gbc_lblRadius = new GridBagConstraints();
+			gbc_lblRadius.anchor = GridBagConstraints.EAST;
+			gbc_lblRadius.insets = new Insets(0, 0, 5, 5);
+			gbc_lblRadius.gridx = 1;
+			gbc_lblRadius.gridy = 3;
+			contentPanel.add(lblRadius, gbc_lblRadius);
+		}
+		{
+			textFieldRadius = new JTextField();
+			textFieldRadius.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+					char c = e.getKeyChar();
+					if(c < '0' || c > '9' || c == KeyEvent.VK_SPACE)
+					{
+						if(c != KeyEvent.VK_MINUS)
+						{
+							e.consume();
+						}
+					}
+				}
+			});
+			GridBagConstraints gbc_textFieldRadius = new GridBagConstraints();
+			gbc_textFieldRadius.insets = new Insets(0, 0, 5, 0);
+			gbc_textFieldRadius.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textFieldRadius.gridx = 2;
+			gbc_textFieldRadius.gridy = 3;
+			contentPanel.add(textFieldRadius, gbc_textFieldRadius);
+			textFieldRadius.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						boolean flag_message = false;
+						try
+						{
+							check_one = Integer.parseInt(textFieldX.getText());
+							check_two = Integer.parseInt(textFieldY.getText());
+							check_three = Integer.parseInt(textFieldRadius.getText());
+						}
+						catch(Exception ex)
+						{
+							JOptionPane.showMessageDialog(null, "Wrong input try again!", "Incorrect input", JOptionPane.ERROR_MESSAGE);
+							flag_message = true;
+						}
+						if(check_one <= 0 || check_two <= 0 || check_three <= 0)
+						{
+							if(!flag_message)
+							{
+							JOptionPane.showMessageDialog(null, "Input can't be equal or lower than 0!", "Incorrect input", JOptionPane.ERROR_MESSAGE);
+							}
+						}
+						else
+						{
+							flag_ok = true;
+							dispose();
+						}
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+
+	}
+	
+	public JTextField getTextFieldRadius() {
+		return textFieldRadius;
+	}
+
+	public JTextField getTextFieldX() {
+		return textFieldX;
+	}
+
+	public JTextField getTextFieldY() {
+		return textFieldY;
+	}
+
+	public boolean isFlag_ok() {
+		return flag_ok;
+	}
+
+	public void setFlag_ok(boolean flag_ok) {
+		this.flag_ok = flag_ok;
+	}
+	public int getCheck_one() {
+		return check_one;
+	}
+
+	public int getCheck_two() {
+		return check_two;
+	}
+
+	public int getCheck_three() {
+		return check_three;
+	}
+
+
+}
---
+++ MH 18-2019 Cosin Stefan/src/geometry/Stack_Sort.java
@@ -0,0 +1,187 @@
+package geometry;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.AbstractListModel;
+
+public class Stack_Sort extends JFrame {
+
+	private JPanel contentPane;
+	private DefaultListModel<Circle> dlm = new DefaultListModel<Circle>();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Stack_Sort frame = new Stack_Sort();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Stack_Sort() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panelNorth = new JPanel();
+		contentPane.add(panelNorth, BorderLayout.NORTH);
+		GridBagLayout gbl_panelNorth = new GridBagLayout();
+		gbl_panelNorth.columnWidths = new int[]{101, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panelNorth.rowHeights = new int[]{0, 0};
+		gbl_panelNorth.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelNorth.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelNorth.setLayout(gbl_panelNorth);
+		
+		JLabel lblTitle = new JLabel("Cosin Stefan MH18-2019");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		gbc_lblTitle.gridx = 6;
+		gbc_lblTitle.gridy = 0;
+		panelNorth.add(lblTitle, gbc_lblTitle);
+		
+		JPanel panelSouth = new JPanel();
+		contentPane.add(panelSouth, BorderLayout.SOUTH);
+		GridBagLayout gbl_panelSouth = new GridBagLayout();
+		gbl_panelSouth.columnWidths = new int[]{168, 51, 0};
+		gbl_panelSouth.rowHeights = new int[]{21, 0};
+		gbl_panelSouth.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelSouth.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelSouth.setLayout(gbl_panelSouth);
+		
+		JButton btnSort = new JButton("Sort");
+		btnSort.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(dlm.size() > 1)
+				{
+					//bubble sort
+					for(int i = 0;i < dlm.size()-1;i++)
+					{
+						for(int j = i + 1;j < dlm.size();j++)
+						{
+							if(dlm.get(i).area() < dlm.get(j).area())
+							{
+								Circle temp = dlm.get(i);
+								dlm.set(i, dlm.get(j));
+								dlm.set(j, temp);
+							}
+						}
+					}
+				}
+				
+			}
+		});
+		GridBagConstraints gbc_btnSort = new GridBagConstraints();
+		gbc_btnSort.anchor = GridBagConstraints.WEST;
+		gbc_btnSort.gridx = 1;
+		gbc_btnSort.gridy = 0;
+		panelSouth.add(btnSort, gbc_btnSort);
+		
+		JPanel panelWest = new JPanel();
+		contentPane.add(panelWest, BorderLayout.WEST);
+		GridBagLayout gbl_panelWest = new GridBagLayout();
+		gbl_panelWest.columnWidths = new int[]{0, 0};
+		gbl_panelWest.rowHeights = new int[]{97, 0, 0, 0, 0};
+		gbl_panelWest.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelWest.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelWest.setLayout(gbl_panelWest);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DlgStack_Sort dlgss = new DlgStack_Sort();
+				dlgss.setVisible(true);
+				if(dlgss.isFlag_ok())
+				{
+					Circle c = new Circle(new Point(dlgss.getCheck_one(), dlgss.getCheck_two()), dlgss.getCheck_three());
+					dlm.add(0, c);
+					dlgss.setFlag_ok(false);
+				}
+			}
+		});
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 3;
+		panelWest.add(btnAdd, gbc_btnAdd);
+		
+		JPanel panelEast = new JPanel();
+		contentPane.add(panelEast, BorderLayout.EAST);
+		GridBagLayout gbl_panelEast = new GridBagLayout();
+		gbl_panelEast.columnWidths = new int[]{0, 0};
+		gbl_panelEast.rowHeights = new int[]{94, 0, 0, 0, 0};
+		gbl_panelEast.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelEast.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelEast.setLayout(gbl_panelEast);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(dlm.size() != 0)
+				{
+					DlgStack_Sort dlgss = new DlgStack_Sort();
+					dlgss.getTextFieldX().enable(false);
+					dlgss.getTextFieldY().enable(false);
+					dlgss.getTextFieldRadius().enable(false);
+					dlgss.getTextFieldX().setText(Integer.toString(dlm.get(0).getCenter().getX()));
+					dlgss.getTextFieldY().setText(Integer.toString(dlm.get(0).getCenter().getY()));
+					dlgss.getTextFieldRadius().setText(Integer.toString(dlm.get(0).getRadius()));
+					dlgss.setVisible(true);
+					if(dlgss.isFlag_ok())
+					{
+						dlm.remove(0);
+						dlgss.setFlag_ok(false);
+					}
+				}
+			}
+		});
+		GridBagConstraints gbc_btnRemove = new GridBagConstraints();
+		gbc_btnRemove.gridx = 0;
+		gbc_btnRemove.gridy = 3;
+		panelEast.add(btnRemove, gbc_btnRemove);
+		
+		JPanel panelCenter = new JPanel();
+		contentPane.add(panelCenter, BorderLayout.CENTER);
+		panelCenter.setLayout(new BorderLayout(0, 0));
+		
+		JList<Circle> list = new JList<Circle>();
+		list.setModel(dlm);
+		panelCenter.add(list, BorderLayout.CENTER);
+	}
+
+}
